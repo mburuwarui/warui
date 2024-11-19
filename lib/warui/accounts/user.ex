0c47cfdb361cc060e6e8c5268f12376225e50b19
mburuwarui <mburuwarui@proton.me>
2024-11-19 21:26:45 +0300

    add_magic_link_auth

--- lib/warui/accounts/user.ex
+++ lib/warui/accounts/user.ex
@@ -12,6 +12,15 @@ defmodule Warui.Accounts.User do
       token_resource Warui.Accounts.Token
       signing_secret Warui.Secrets
     end
+
+    strategies do
+      magic_link do
+        identity_field :email
+        registration_enabled? true
+
+        sender Warui.Accounts.User.Senders.SendMagicLinkEmail
+      end
+    end
   end
 
   postgres do
@@ -25,6 +34,45 @@ defmodule Warui.Accounts.User do
       argument :subject, :string, allow_nil?: false
       get? true
       prepare AshAuthentication.Preparations.FilterBySubject
+    end
+
+    read :get_by_email do
+      description "Looks up a user by their email"
+      get? true
+
+      argument :email, :ci_string do
+        allow_nil? false
+      end
+
+      filter expr(email == ^arg(:email))
+    end
+
+    create :sign_in_with_magic_link do
+      description "Sign in or register a user with magic link."
+
+      argument :token, :string do
+        description "The token from the magic link that was sent to the user"
+        allow_nil? false
+      end
+
+      upsert? true
+      upsert_identity :unique_email
+      upsert_fields [:email]
+
+      # Uses the information from the token to create or sign in the user
+      change AshAuthentication.Strategy.MagicLink.SignInChange
+
+      metadata :token, :string do
+        allow_nil? false
+      end
+    end
+
+    action :request_magic_link do
+      argument :email, :ci_string do
+        allow_nil? false
+      end
+
+      run AshAuthentication.Strategy.MagicLink.Request
     end
   end
 
@@ -40,5 +88,14 @@ defmodule Warui.Accounts.User do
 
   attributes do
     uuid_primary_key :id
+
+    attribute :email, :ci_string do
+      allow_nil? false
+      public? true
+    end
+  end
+
+  identities do
+    identity :unique_email, [:email]
   end
 end

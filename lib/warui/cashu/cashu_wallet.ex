67ee0dba4ed214b919d077ee4b4cc68280250951
mburuwarui <mburuwarui@proton.me>
2024-12-16 04:31:48 +0300

    rustler_test_pass

---
+++ lib/warui/cashu/cashu_wallet.ex
@@ -0,0 +1,6 @@
+defmodule CashuWallet do
+  use Rustler, otp_app: :warui, crate: "cashu_wallet"
+
+  # When your NIF is loaded, it will override this function.
+  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+end

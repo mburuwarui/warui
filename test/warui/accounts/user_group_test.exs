fdb000af1b6c8115bb95692a8b147428b3cbf095
mburuwarui <mburuwarui@proton.me>
2025-05-09 01:16:43 +0300

    add_user_permission_module_test

---
+++ test/warui/accounts/user_group_test.exs
@@ -0,0 +1,31 @@
+defmodule Warui.Accounts.UserGroupTest do
+  use WaruiWeb.ConnCase, async: false
+  require Ash.Query
+
+  describe "User Access Group Tests" do
+    test "Group can be added to a user" do
+      # Prepare data
+      user = create_user("John")
+      group_attrs = %{name: "Accountants", description: "Can manage billing in the system"}
+      group = Ash.create!(Warui.Accounts.Group, group_attrs, actor: user)
+
+      # Attempt to link group to permission
+      user_group_attrs = %{group_id: group.id, user_id: user.id}
+
+      user_group =
+        Ash.create!(
+          Warui.Accounts.UserGroup,
+          user_group_attrs,
+          actor: user,
+          load: [:group, :user],
+          # Set off authorize so we can auto-load user relationshp
+          authorize?: false
+        )
+
+      # Confirm that the association happened and in the right tenant
+      assert user.current_organization == Ash.Resource.get_metadata(user_group, :tenant)
+      assert user_group.user.id == user.id
+      assert user_group.group.id == group.id
+    end
+  end
+end

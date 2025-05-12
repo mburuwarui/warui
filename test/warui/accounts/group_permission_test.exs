a475048cc0689f6fb73e3af806dff4f1ecf9d247
mburuwarui <mburuwarui@proton.me>
2025-05-12 12:21:32 +0300

    refactor_permission_tests

--- test/warui/accounts/group_permission_test.exs
+++ test/warui/accounts/group_permission_test.exs
@@ -5,27 +5,24 @@ defmodule Warui.Accounts.GroupPermissionTest do
   describe "Access Group Permission Tests" do
     test "Permission can be added to a group" do
       # Prepare data
-      perm_attr = %{action: "read", resource: "category"}
-      permission = Ash.create!(Warui.Accounts.Permission, perm_attr)
-
       user = create_user("Joe")
       group_attrs = %{name: "Accountants", description: "Can manage billing in the system"}
       group = Ash.create!(Warui.Accounts.Group, group_attrs, actor: user)
 
       # Attempt to link group to permission
-      group_perm_attrs = %{group_id: group.id, permission_id: permission.id}
+      group_perm_attrs = %{action: "read", resource: "ledger", group_id: group.id}
 
       group_perm =
         Ash.create!(
           Warui.Accounts.GroupPermission,
           group_perm_attrs,
           actor: user,
-          load: [:group, :permission]
+          load: [:group]
         )
 
       # Confirm that the association happened and in the right tenant
       assert user.current_organization == Ash.Resource.get_metadata(group_perm, :tenant)
-      assert group_perm.permission.id == permission.id
+      assert group_perm.action == "read"
       assert group_perm.group.id == group.id
     end
   end
